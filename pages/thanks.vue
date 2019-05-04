f72e0e5f673f55e43a7aa0387b30b506ed759bfb
Ramkumar Shankar <ramkumarshankr@gmail.com>
2019-05-04 19:06:01 +1000

    add thank you page

---
+++ pages/thanks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="thanks">
+    <div class="container">
+      <h1>Thanks!</h1>
+      <p>I'll get back to you as soon as possible</p>
+      <nuxt-link to="/">Go back home</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+.thanks {
+  margin-bottom: 100px;
+
+  p {
+    margin-bottom: 50px;
+  }
+}
+
+</style>
