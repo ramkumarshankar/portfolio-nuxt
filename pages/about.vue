2ad278afcb004e90ee6829bd113f3bcb6127b0fe
Ramkumar Shankar <ramkumarshankr@gmail.com>
2019-05-04 18:44:23 +1000

    about page works

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>About</h1>
+  <div class="about">
+    <div class="container">
+      <h1 class="page-headline">{{ $prismic.richTextAsPlain(title) }}</h1>
+      <section class="about-section">
+        <div>
+          <prismic-rich-text :field="highlight"/>
+        </div>
+        <div>
+          <prismic-rich-text :field="bodyText"/>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  head() {
+    return {
+      titleTemplate: '%s | About'
+    }
+  },
+  async asyncData({app, error, req}) {
+    try {
+      const result = await app.api.getSingle("aboutpage")
+      const document = result.data
+      const docId = result.id;
+      const title = document.title;
+      const highlight = document.highlight;
+      const bodyText = document.text;
+      return {
+        docId: docId,
+        title: title,
+        highlight: highlight,
+        bodyText: bodyText
+      };
+    } catch (e) {
+      console.log(e)
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+h1.page-headline {
+  margin-top: 50px;
+  margin-bottom: 20px;
+}
 
+section.about-section {
+  position: relative;
+  column-gap: 20px;
+  grid-column-gap: 20px;
+  row-gap: 20px;
+  grid-row-gap: 20px;
+  min-height: 300px;
+  display: grid;
+  grid-template-columns: 0.33fr 0.66fr;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr !important;
+  }
+}
 </style>
