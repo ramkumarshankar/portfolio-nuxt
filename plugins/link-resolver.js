1e32f4fa24ff2802be63206c5e51999e90d33001
Ramkumar Shankar <ramkumarshankr@gmail.com>
2019-05-04 19:04:59 +1000

    fix link resolver homepage type

--- plugins/link-resolver.js
+++ plugins/link-resolver.js
@@ -1,7 +1,7 @@
 export default doc => {
   // Return the path depending on Prismic Document's type
   // If it is a Single Custom Type with the API ID of "home"
-  if (doc.type === 'home') {
+  if (doc.type === 'homepage') {
     return '/'
   }
   // If it is a Repeatable Custom Type with the API ID of "project" (must contain a UID field)
