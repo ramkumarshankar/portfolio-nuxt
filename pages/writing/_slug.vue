e2f4053b8125630fda54bc1755d73cd09b5df6fc
Ramkumar Shankar <1174154+ramkumarshankar@users.noreply.github.com>
2020-08-05 10:09:48 +1000

    blockquotes can now have a source

--- pages/writing/_slug.vue
+++ pages/writing/_slug.vue
@@ -9,12 +9,13 @@
       class="article-slice"
     >
       <template v-if="slice.slice_type === 'rich_text'">
-        <prismic-rich-text :field="slice.primary.content"/>
+        <prismic-rich-text :field="slice.primary.content" />
       </template>
       <template v-else-if="slice.slice_type === 'blockquote'">
-        <blockquote v-for="(item, blockquoteIndex) in slice.items" :key="blockquoteIndex">
-          <prismic-rich-text :field="item.content"/>
+        <blockquote>
+          <prismic-rich-text :field="slice.primary.content" />
         </blockquote>
+        <prismic-rich-text v-if="slice.primary.source.length > 0" :field="slice.primary.source" />
       </template>
       <template v-else-if="slice.slice_type === 'code_snippet'">
         <div v-highlight v-for="(item, codeBlockIndex) in slice.items" :key="'codeBlock-' + codeBlockIndex">
